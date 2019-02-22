3461987e2057eabe5ad24bfe83adcff0cbc86b42
Nick Underwood <nunderwood6@gmail.com>
2019-02-21 22:17:52 -0600

    updated heat

--- js/lib/leaflet-heatmap.js
+++ js/lib/leaflet-heatmap.js
@@ -84,9 +84,15 @@
 
       bounds = this._map.getBounds();
       zoom = this._map.getZoom();
+      //hack
+ 
+      if(zoom==8){
+        scale = Math.pow(1.98, zoom);
+      }
+      else{
+        scale = Math.pow(1.98, zoom);
+      }
     
-        scale = Math.pow(1.99, zoom);
-      
 
       if (this._data.length == 0) {
         if (this._heatmap) {
